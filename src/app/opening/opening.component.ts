041b1a36c681d91a30fa23d47fcd930862736f5b
Jesus Medina <medinajesus821@gmail.com>
2020-01-31 16:46:22 -0400

    sonido, buscador

--- src/app/opening/opening.component.ts
+++ src/app/opening/opening.component.ts
@@ -1,17 +1,31 @@
-import { Component, Input, OnChanges, SimpleChanges, Output, EventEmitter } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges, Output, EventEmitter, OnDestroy } from '@angular/core';
+import {trigger, style, animate, transition} from '@angular/animations';
 
 @Component({
   selector: 'app-opening',
   templateUrl: './opening.component.html',
-  styleUrls: ['./opening.component.scss']
+  styleUrls: ['./opening.component.scss'],
+  animations: [
+    trigger('fade', [ 
+      transition('void => *', [
+        style({ opacity: 0 }), 
+        animate(1000, style({opacity: 1}))
+      ]),
+      transition('* => closed', [
+        style({ opacity: 1 }), 
+        animate(600, style({opacity: 0}))
+      ])
+    ])
+  ]
 })
-export class OpeningComponent implements OnChanges {
+export class OpeningComponent implements OnChanges, OnDestroy {
   
   @Input('crawl') opening_crawl : Array<any> = [];
   @Input('episode') episode : Array<any>;
   @Input('title') title: string = '';
   @Input('stars') numStars: number = 0;
   @Output('onclose') closeClick = new EventEmitter();
+  audio = new Audio();
   constructor() { }
 
   ngOnInit() {
@@ -21,8 +35,9 @@ export class OpeningComponent implements OnChanges {
         var xy = this.getRandomPosition();
             star.style.top = xy[0] + 'px';
             star.style.left = xy[1] + 'px';
-        document.body.append(star);
-        }
+        document.getElementById('opening-container').append(star);
+    }
+    setTimeout(() => this.playIntro(), 2000);
     }
 
   ngOnChanges(changes: SimpleChanges) {
@@ -37,16 +52,34 @@ export class OpeningComponent implements OnChanges {
     }
   }
 
+  ngOnDestroy() {
+    this.removeStars();
+    this.audio.src = '';
+  }
+
+  removeStars() {
+    const stars = document.getElementsByClassName('star');
+    for (let i = 0; i < stars.length; i++) {
+      stars.item(i).remove();
+    }
+  }
+
   closeMe() {
     this.closeClick.emit(true);
   }
 
-getRandomPosition() {  
-  var y = window.innerWidth;
-  var x = window.innerHeight;
-    var randomX = Math.floor(Math.random()*x);
-    var randomY = Math.floor(Math.random()*y);
-    return [randomX,randomY];
-}
+  getRandomPosition() {  
+    var y = window.innerWidth;
+    var x = window.innerHeight;
+      var randomX = Math.floor(Math.random()*x);
+      var randomY = Math.floor(Math.random()*y);
+      return [randomX,randomY];
+  }
+
+  playIntro() {
+    this.audio.src = "assets/intro.mp3";
+    this.audio.load();
+    this.audio.play();
+  }
 
 }
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Movie } from '../models/movie';
-import { Observable } from 'rxjs';
+import { Observable, forkJoin } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +14,11 @@ export class MovieService {
 
   getMovies(): Observable<Movie> {
     return this.http.get<Movie>("https://swapi.co/api/films/");
+  }
+
+  getCharacters(urls: Array<string>): Observable<any> {
+    const requests = urls.map(url => this.http.get(url));
+    return forkJoin(requests);
   }
 
 }
